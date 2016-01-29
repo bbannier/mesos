c4cbc11d4ea99d3eaf92de20fd2aa50899b158c8
Benjamin Bannier <benjamin.bannier@mesosphere.io>
2016-01-28 18:28:15 -0800

    fixup! Updated the unallocation to use the active role of a resource.

--- src/master/allocator/mesos/hierarchical.cpp
+++ src/master/allocator/mesos/hierarchical.cpp
@@ -936,7 +936,13 @@ void HierarchicalAllocatorProcess::recoverResources(
             << " for " << timeout.get();
 
     // Create a new filter.
-    OfferFilter* offerFilter = new RefusedOfferFilter(resources);
+    // The active_role is irrelevant for OfferFilters.
+    Resources resources_;
+    foreach(Resource resource, resources) {
+      resource.clear_active_role();
+      resources_ += resource;
+    }
+    OfferFilter* offerFilter = new RefusedOfferFilter(resources_);
     frameworks[frameworkId].offerFilters[slaveId].insert(offerFilter);
 
     // We need to disambiguate the function call to pick the correct
