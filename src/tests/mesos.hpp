592c705206cf8a7cb85d443d6b20391e147ddd1f
Jan Schlicht <jan@mesosphere.io>
2016-04-22 12:19:10 +0200

    Added agent authorization flags.
    See summary.
    
    Review: https://reviews.apache.org/r/45922/

--- src/tests/mesos.hpp
+++ src/tests/mesos.hpp
@@ -1252,7 +1252,8 @@ public:
       const slave::Flags& flags,
       mesos::master::detector::MasterDetector* detector,
       slave::Containerizer* containerizer,
-      const Option<mesos::slave::QoSController*>& qosController = None());
+      const Option<mesos::slave::QoSController*>& qosController = None(),
+      const Option<mesos::Authorizer*>& authorizer = None());
 
   virtual ~MockSlave();
 
