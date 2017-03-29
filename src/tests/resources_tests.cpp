d2c622e64e2cf1dccd80bd18b0d4f9290036199e
Jan Schlicht <jan@mesosphere.io>
2017-03-29 12:52:31 +0200

    Added resource support for new offer operations.
    When applying offer operations to resources, one has to account for
    changing resources depending on the operation. For resource-related
    operations, disk resources will be converted from a type to another.
    So, by applying such an operation, we will substract the resource
    of the source type and add the resource of the target type of that
    operation.
    
    Review: https://reviews.apache.org/r/58072

--- src/tests/resources_tests.cpp
+++ src/tests/resources_tests.cpp
@@ -2690,6 +2690,88 @@ TEST(ResourcesOperationTest, CreateSharedPersistentVolume)
 }
 
 
+TEST(ResourcesOperationTest, CreateVolume)
+{
+  Resource source = createDiskResource(
+      "1000",
+      "role",
+      None(),
+      "path",
+      createDiskSourceRaw());
+
+  Offer::Operation create_volume;
+  create_volume.set_type(Offer::Operation::CREATE_VOLUME);
+  create_volume.mutable_create_volume()->mutable_source()->CopyFrom(source);
+  create_volume.mutable_create_volume()->set_target_type(
+      Resource::DiskInfo::Source::PATH);
+
+  Resources other = Resources::parse("cpus:1;mem:512").get();
+
+  Resource volume = createDiskResource(
+      "1000",
+      "role",
+      None(),
+      "path",
+      createDiskSourcePath());
+
+  EXPECT_SOME_EQ(
+      (other + volume),
+      (other + source).apply(create_volume));
+
+  Offer::Operation destroy_volume;
+  destroy_volume.set_type(Offer::Operation::DESTROY_VOLUME);
+  destroy_volume.mutable_destroy_volume()->mutable_volume()->CopyFrom(volume);
+
+  EXPECT_SOME_EQ(
+      (other + source),
+      (other + volume).apply(destroy_volume));
+
+  EXPECT_SOME_EQ(
+      (other + source),
+      (other + source).apply(create_volume)->apply(destroy_volume));
+}
+
+
+TEST(ResourcesOperationTest, CreateBlock)
+{
+  Resource source = createDiskResource(
+      "1000",
+      "role",
+      None(),
+      "path",
+      createDiskSourceRaw());
+
+  Offer::Operation create_block;
+  create_block.set_type(Offer::Operation::CREATE_BLOCK);
+  create_block.mutable_create_block()->mutable_source()->CopyFrom(source);
+
+  Resources other = Resources::parse("cpus:1;mem:512").get();
+
+  Resource block = createDiskResource(
+      "1000",
+      "role",
+      None(),
+      "path",
+      createDiskSourceBlock());
+
+  EXPECT_SOME_EQ(
+      (other + block),
+      (other + source).apply(create_block));
+
+  Offer::Operation destroy_block;
+  destroy_block.set_type(Offer::Operation::DESTROY_BLOCK);
+  destroy_block.mutable_destroy_block()->mutable_block()->CopyFrom(block);
+
+  EXPECT_SOME_EQ(
+      (other + source),
+      (other + block).apply(destroy_block));
+
+  EXPECT_SOME_EQ(
+      (other + source),
+      (other + source).apply(create_block)->apply(destroy_block));
+}
+
+
 TEST(ResourcesOperationTest, FlattenResources)
 {
   Resources unreservedCpus = Resources::parse("cpus:1").get();
