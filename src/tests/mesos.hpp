f56d93cbc25afd9617b1a48aeafb54a99eee4db5
Jan Schlicht <jan@mesosphere.io>
2017-03-30 15:16:09 +0200

    Updated test helpers to support changed disk types.
    In addition to creating BLOCK and RAW disk sources,
    PATH and MOUNT disk sources can now be created without a root path.
    
    Review: https://reviews.apache.org/r/58071

--- src/tests/mesos.hpp
+++ src/tests/mesos.hpp
@@ -806,12 +806,14 @@ inline typename TResource::DiskInfo createDiskInfo(
 // Helper for creating a disk source with type `PATH`.
 template <typename TResource>
 inline typename TResource::DiskInfo::Source createDiskSourcePath(
-    const std::string& root)
+    const Option<std::string>& root = None())
 {
   typename TResource::DiskInfo::Source source;
 
   source.set_type(TResource::DiskInfo::Source::PATH);
-  source.mutable_path()->set_root(root);
+  if (root.isSome()) {
+    source.mutable_path()->set_root(root.get());
+  }
 
   return source;
 }
@@ -820,12 +822,38 @@ inline typename TResource::DiskInfo::Source createDiskSourcePath(
 // Helper for creating a disk source with type `MOUNT`.
 template <typename TResource>
 inline typename TResource::DiskInfo::Source createDiskSourceMount(
-    const std::string& root)
+    const Option<std::string>& root = None())
 {
   typename TResource::DiskInfo::Source source;
 
   source.set_type(TResource::DiskInfo::Source::MOUNT);
-  source.mutable_mount()->set_root(root);
+  if (root.isSome()) {
+    source.mutable_mount()->set_root(root.get());
+  }
+
+  return source;
+}
+
+
+// Helper for creating a disk source with type `BLOCK`.
+template <typename TResource>
+inline typename TResource::DiskInfo::Source createDiskSourceBlock()
+{
+  typename TResource::DiskInfo::Source source;
+
+  source.set_type(TResource::DiskInfo::Source::BLOCK);
+
+  return source;
+}
+
+
+// Helper for creating a disk source with type `RAW`.
+template <typename TResource>
+inline typename TResource::DiskInfo::Source createDiskSourceRaw()
+{
+  typename TResource::DiskInfo::Source source;
+
+  source.set_type(TResource::DiskInfo::Source::RAW);
 
   return source;
 }
@@ -1193,6 +1221,20 @@ inline Resource::DiskInfo::Source createDiskSourceMount(Args&&... args)
 
 
 template <typename... Args>
+inline Resource::DiskInfo::Source createDiskSourceBlock(Args&&... args)
+{
+  return common::createDiskSourceBlock<Resource>(std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+inline Resource::DiskInfo::Source createDiskSourceRaw(Args&&... args)
+{
+  return common::createDiskSourceRaw<Resource>(std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
 inline Resource createDiskResource(Args&&... args)
 {
   return common::createDiskResource<Resource, Resources, Volume, Labels>(
@@ -1403,6 +1445,24 @@ inline mesos::v1::Resource::DiskInfo::Source createDiskSourceMount(
     Args&&... args)
 {
   return common::createDiskSourceMount<mesos::v1::Resource>(
+      std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+inline mesos::v1::Resource::DiskInfo::Source createDiskSourceBlock(
+    Args&&... args)
+{
+  return common::createDiskSourceBlock<mesos::v1::Resource>(
+      std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+inline mesos::v1::Resource::DiskInfo::Source createDiskSourceRaw(
+    Args&&... args)
+{
+  return common::createDiskSourceRaw<mesos::v1::Resource>(
       std::forward<Args>(args)...);
 }
 
